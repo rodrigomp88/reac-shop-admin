d2da893ceed30e54b120a0b4807026938bd46d6b
Rodrigo <rodrigomp88@gmail.com>
2022-03-17 23:41:10 -0300

    dashboadr

---
+++ src/pages/_app.js
@@ -0,0 +1,17 @@
+import { ProviderAuth } from '@hooks/useAuth';
+import '@styles/tailwind.css';
+import MainLayout from '@layout/MainLayout';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <ProviderAuth>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ProviderAuth>
+    </>
+  );
+}
+
+export default MyApp;
